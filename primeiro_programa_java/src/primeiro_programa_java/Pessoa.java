9d2dd6869f75379efed51802d2b41d25835aa4bc
kezialacerda <kezialacerdas@gmail.com>
2023-02-03 16:32:16 -0300

    ALteração e criação da classe pessoa

--- primeiro_programa_java/src/primeiro_programa_java/Pessoa.java
+++ primeiro_programa_java/src/primeiro_programa_java/Pessoa.java
@@ -2,4 +2,28 @@ package primeiro_programa_java;
 
 public class Pessoa {
 
+	private String nome;
+	private Integer idade;
+	private String dataNascimento;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Integer getIdade() {
+		return idade;
+	}
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
 }
