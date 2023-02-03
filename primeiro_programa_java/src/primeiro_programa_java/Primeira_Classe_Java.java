d9130b9296547526d2c9f7ee527f6c8d520f5783
kezialacerda <kezialacerdas@gmail.com>
2023-02-03 15:35:01 -0300

    Criação do Projeto java 1

---
+++ primeiro_programa_java/src/primeiro_programa_java/Primeira_Classe_Java.java
@@ -0,0 +1,10 @@
+package primeiro_programa_java;
+
+public class Primeira_Classe_Java {
+ /* main é um método auto executável em java*/
+	public static void main(String[] args) {
+		
+		/*Escreve texto no console*/
+		System.out.println("Meu primeiro programa em java");
+	}
+}
