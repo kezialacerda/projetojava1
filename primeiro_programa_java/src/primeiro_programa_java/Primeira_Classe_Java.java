0d4e6f2006dd936ea1d390157667980d331700f0
kezialacerda <kezialacerdas@gmail.com>
2023-08-29 20:29:57 -0300

    Primeira Classe

--- primeiro_programa_java/src/primeiro_programa_java/Primeira_Classe_Java.java
+++ primeiro_programa_java/src/primeiro_programa_java/Primeira_Classe_Java.java
@@ -4,7 +4,6 @@ public class Primeira_Classe_Java {
  /* main é um método auto executável em java*/
 	public static void main(String[] args) {
 		
-		/*Escreve texto no console*/
-		System.out.println("Meu primeiro programa em java");
+	
 	}
 }
